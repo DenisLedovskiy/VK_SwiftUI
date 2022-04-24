dd5f3e6f00e53d83a1df7c6680701bd1b33c7b43
DenisLedovskiy <BigDen_07@bk.ru>
2022-04-24 18:11:14 +0300

    Initial Commit

---
+++ VK(SwiftUI)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VK(SwiftUI)
+//
+//  Created by Денис Ледовский on 24.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VK(SwiftUI)/VK_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  VK_SwiftUI_App.swift
+//  VK(SwiftUI)
+//
+//  Created by Денис Ледовский on 24.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct VK_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
